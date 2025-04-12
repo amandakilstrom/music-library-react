68d065587cac757d7e77a1f623eed52a27f5c595
Amanda Kilström <Amanda.kilstrom@hotmail.com>
2025-04-12 14:10:16 +0200

    lördag

---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export function Footer() {
+    return (
+        <div class="container">
+        <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+            <div class="col-md-4 d-flex align-items-center">
+                <a href="./album.html" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
+                    <span class="mb-3 mb-md-0 text-body-secondary">&copy; Amanda Kilström</span>
+                </a>
+            </div>
+        </footer>
+      </div>
+    )
+}
---
+++ src/pages/home.js
@@ -0,0 +1,14 @@
+import react from 'react';
+
+export function Home() {
+    return (
+        <div class="container px-4 py-4" id="home">
+        <div class="bg-body-tertiary p-5 rounded">
+          <div class="col-sm-8 py-5 mx-auto">
+            <h1 class="display-5 fw-normal">Welcome to the Music Library!</h1>
+              <p class="fs-5">Here you can find different music groups and their albums</p>
+          </div>
+        </div>
+      </div>
+    )
+}
