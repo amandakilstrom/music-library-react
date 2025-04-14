96313d5c4148ed4bcd56dd8b120bf9d717f83469
Amanda Kilström <Amanda.kilstrom@hotmail.com>
2025-04-14 18:59:04 +0200

    igen

--- src/pages/music-groups.js
+++ src/pages/music-groups.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 export default function MusicGroups() {
+    
+    MusicGroupFetch();
+    
     return (
         <h1>Music Groups</h1>
+
+
     )
+
+
+}
+
+async function MusicGroupFetch() {
+    const respone = await fetch("https://seido-webservice-307d89e1f16a.azurewebsites.net/api/MusicGroup/Read?seeded=true&flat=true&pageNr=0&pageSize=10");
+    const data = await respone.json();
+
+    console.log(data);
 }
