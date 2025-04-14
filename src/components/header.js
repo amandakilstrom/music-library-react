e947f6ba1e2964bc2a3570b8b54397311dfbeca5
Amanda Kilström <Amanda.kilstrom@hotmail.com>
2025-04-14 18:05:21 +0200

    .

--- src/components/header.js
+++ src/components/header.js
@@ -5,20 +5,20 @@ export function Header() {
     return (
 
         <div className="container">
-        <NavMenu />
+            <NavMenu />
         </div>
 
-    //     <div class="container">
-    //     <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-    //         <a href="../home.js" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-    //             <span class="fs-4">The Music Library</span>
-    //         </a>
+        //     <div class="container">
+        //     <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+        //         <a href="../home.js" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+        //             <span class="fs-4">The Music Library</span>
+        //         </a>
 
-    //         <ul class="nav nav-pills">
-    //             <li class="nav-item"><a href="../home.js" class="nav-link">Home</a></li>
-    //             <li class="nav-item"><a href="../music-groups.js" class="nav-link">Music Groups</a></li>
-    //         </ul>
-    //     </header>
-    //   </div>
+        //         <ul class="nav nav-pills">
+        //             <li class="nav-item"><a href="../home.js" class="nav-link">Home</a></li>
+        //             <li class="nav-item"><a href="../music-groups.js" class="nav-link">Music Groups</a></li>
+        //         </ul>
+        //     </header>
+        //   </div>
     )
 }
