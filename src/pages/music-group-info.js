478b93290b61cf40f70a66ae3c0270d5da742cd2
Amanda Kilström <Amanda.kilstrom@hotmail.com>
2025-04-15 16:40:49 +0200

    .

--- src/pages/music-group-info.js
+++ src/pages/music-group-info.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 
-import musicService from "../services/music-group-service";
+// import musicService from "../services/music-group-service";
 
 export default function MusicGroupInfo() {
     
-    const { musicGroupId } = useParams();
-    console.log('musicGroupId:', musicGroupId);
+    // const { musicGroupId } = useParams();
+    // console.log('musicGroupId:', musicGroupId);
     
     return (
+        <>
         <h1>hej </h1>
+        </>
     )
 }
--- src/routes/approuter.js
+++ src/routes/approuter.js
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router";
 
 import Home from '../pages/home.js';
 import MusicGroups from '../pages/music-groups';
-import MusicGroupInfo from '../pages/music-group-info';
+import MusicGroupInfo from '../pages/music-group-info.js';
 
 export function AppRouter() {
     return (
